cb9783395bb73fda5612057af4e9b22ed9923581
anav7026 <anav7026@gmail.com>
2025-04-23 14:40:18 -0400

    Delete pythonProject directory

--- pythonProject/TaxDocument.py
+++
@@ -1,7 +0,0 @@
-
-class TaxDocument:
-    def __init__(self, form_name, due_date,description,form_pdf):
-        self.form_name=form_name
-        self.due_date=due_date  
-        self.description=description
-        self.form_pdf=form_pdf
